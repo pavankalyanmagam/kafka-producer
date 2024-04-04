770a3700d1baf30ce83ce3174e15df4991b5615d
Pavankalyan Magam <pavankalyanmagam@gmail.com>
2024-04-04 18:55:32 -0500

    created kafka producer

---
+++ src/main/java/com/javatechie/kafkaproducer/config/KafkaProducerConfig.java
@@ -0,0 +1,15 @@
+package com.javatechie.kafkaproducer.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic("javatechie-demo-2", 5, (short) 1);
+    }
+}
---
+++ src/main/java/com/javatechie/kafkaproducer/controller/EventController.java
@@ -0,0 +1,30 @@
+package com.javatechie.kafkaproducer.controller;
+
+import com.javatechie.kafkaproducer.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message) {
+        try {
+            for (int i = 0; i < 10000; i++) {
+                kafkaMessagePublisher.sendMessageToTopic(message + " : " + i);
+            }
+            return ResponseEntity.ok("Message published successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
